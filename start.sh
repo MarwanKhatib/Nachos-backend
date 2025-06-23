f10f3f3e41421a4214dc3b46ac09d6ceff586440
Marwan Alkhatib <blackxxxx.predator@gmail.com>
2025-06-23 22:40:34 +0300

    fix start.sh remove migrations

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-set -e  # Exit on error
-
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 echo "Starting Gunicorn..."
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
