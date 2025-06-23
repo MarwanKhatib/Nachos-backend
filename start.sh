81c202e0b5bec80c400a87a072cbad5508e4536e
Marwan Alkhatib <blackxxxx.predator@gmail.com>
2025-06-23 22:17:50 +0300

    remove start.sh from docker

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Start Django with Gunicorn
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000}
